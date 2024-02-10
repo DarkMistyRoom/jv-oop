964c8ae8a3bb21d75dd751e6c3a62f063637f755
Mykhailo Dzhemula <mykhailo.dzhemula@nordstrom.com>
2024-02-10 17:25:21 +0200

    solution

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,15 @@
 package core.basesyntax;
 
 public class MainApp {
+    public static void main() {
+        Machine[] arr = new Machine[3];
+        arr[0] = new Bulldozer();
+        arr[1] = new Excavator();
+        arr[2] = new Truck();
 
+        for (Machine machine: arr) {
+            machine.doWork();
+            machine.stopWork();
+        }
+    }
 }
